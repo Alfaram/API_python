ddb8515c3ea20e8781f785366036c8d560671b32
Max Tuzov <tuzovm692@gmail.com>
2022-05-25 22:52:15 +0400

    1

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-import requests
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-from settings import token
+import logging
+import os
 
-updater = Updater(token=token)
+import requests
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+secret_token = os.getenv('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+
 URL = 'https://api.thecatapi.com/v1/images/search'
 
 
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        # Печатать информацию в консоль теперь не нужно:
+        # всё необходимое будет в логах
+        # print(error)
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
@@ -21,20 +44,26 @@ def new_cat(update, context):
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
-    # За счёт параметра resize_keyboard=True сделаем кнопки поменьше
     button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
 
     context.bot.send_message(
         chat_id=chat.id,
-        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
         reply_markup=button
     )
 
     context.bot.send_photo(chat.id, get_new_image())
 
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+def main():
+    updater = Updater(token=secret_token)
 
-updater.start_polling()
-updater.idle()
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
